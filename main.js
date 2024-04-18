db9d7658f2ef6521c957941063c134d1b00d6ca2
Ylenia <05ylenia@gmail.com>
2024-04-18 18:02:19 +0200

    Hotels und Unterkünfte hinzugefügt + Namen verbessert

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ let themaLayer = {
   lines: L.featureGroup().addTo(map),
   stops: L.featureGroup().addTo(map),
   Fußgängerzonen: L.featureGroup().addTo(map),
+  hotels: L.featureGroup().addTo(map)
 }
 
 // Hintergrundlayer
@@ -34,9 +35,10 @@ L.control
     "Esri World Street Map": L.tileLayer.provider("Esri.WorldStreetMap"),
   }, {
     "Sehenswürdigkeiten": themaLayer.sights,
-    "Vienna Sightseeing Linien": themaLayer.lines,
-    "Vienna Sightseeing Stops": themaLayer.stops,
-    "Vienna Fußgängerzonen": themaLayer.Fußgängerzonen,
+    "Touristische Kraftfahrlinien Liniennetz Vienna Sightseeing Linie Wien ": themaLayer.lines,
+    "Touristische Kraftfahrlinien Haltestellen Vienna Sightseeing Linie Standorte Wien": themaLayer.stops,
+    "Fußgängerzonen Wien": themaLayer.Fußgängerzonen,
+    "Hotels und Unterkünfte Standort Wien": themaLayer.hotels,
   })
   .addTo(map);
 
@@ -135,6 +137,26 @@ async function loadFußgängerzonen(url) {
 
 }
 loadFußgängerzonen("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json")
+
+async function loadHotels(url) {
+  //console.log("Loading", url);
+  let response = await fetch(url);
+  let geojson = await response.json();
+  //console.log(geojson);
+  L.geoJSON(geojson, {
+    onEachFeature: function (feature, layer) {
+      console.log(feature);
+      console.log(feature.properties.NAME);
+      layer.bindPopup(`
+      <img src="${feature.properties.THUMBNAIL}" alt"*">
+      <h4><a href="${feature.properties.WEITERE_INF}" target="wien">${feature.properties.NAME}</a></h4>
+      <adress>${feature.properties.ADRESSE}</adress>      
+      `)
+    }
+  }).addTo(themaLayer.hotels);
+
+}
+loadHotels("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:UNTERKUNFTOGD&srsName=EPSG:4326&outputFormat=json")
 
 /*
 "sightseeing"
