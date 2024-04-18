9e82805607ef72303f7b33e1ed53dd56134411a2
Ylenia <05ylenia@gmail.com>
2024-04-18 17:47:59 +0200

    Lines zu den Sehenswürdigkeiten hinzugefügt

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ startLayer.addTo(map);
 
 let themaLayer = {
   sights: L.featureGroup().addTo(map),
+  lines: L.featureGroup().addTo(map),
 }
 
 // Hintergrundlayer
@@ -31,14 +32,16 @@ L.control
     "Esri World Street Map": L.tileLayer.provider("Esri.WorldStreetMap"),
   }, {
     "Sehenswürdigkeiten": themaLayer.sights,
+    "Vienna Sightseeing Linien": themaLayer.lines
   })
   .addTo(map);
 
-// Marker Stephansdom
+/* Marker Stephansdom
 L.marker([stephansdom.lat, stephansdom.lng])
   .addTo(map)
   .bindPopup(stephansdom.title)
   .openPopup();
+*/
 
 // Maßstab
 L.control
@@ -68,3 +71,43 @@ async function loadSights(url) {
 
 }
 loadSights("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json")
+
+async function loadLines(url) {
+  //console.log("Loading", url);
+  let response = await fetch (url);
+  let geojson = await response.json();
+  //console.log(geojson);
+  L.geoJSON(geojson, {
+    onEachFeature: function (feature, layer) {
+      console.log(feature);
+      console.log(feature.properties.NAME);
+      layer.bindPopup(`
+      <img src="${feature.properties.THUMBNAIL}" alt"*">
+      <h4><a href="${feature.properties.WEITERE_INF}" target="wien">${feature.properties.NAME}</a></h4>
+      <adress>${feature.properties.ADRESSE}</adress>      
+      `)
+    }
+  }).addTo(themaLayer.lines);
+
+}
+loadLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json")
+/*
+"sightseeing"
+
+loadLines
+Touristische Kraftfahrlinien Liniennetz Vienna Sightseeing Linie Wien 
+
+loadStops
+Touristische Kraftfahrlinien Haltestellen Vienna Sightseeing Linie Standorte Wien
+
+"fußgängerzone"
+loadZones
+Fußgängerzonen Wien
+zones
+
+"hotel"
+loadHotels
+Hotels und Unterkünfte Standort Wien
+hotels
+
+*/
